6239609ac04510ac6d851bffd030f2e53f518a1b
Bartosz Nowakowski <bartosz.wojciech.nowakowski@gmail.com>
2024-06-12 16:48:40 +0200

    3rd commit konstruktor

--- src/Library.java
+++ src/Library.java
@@ -1,22 +1,15 @@
-public class Library {
+class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.2";
+        final String appName = "Biblioteka v0.3";
 
-        Book book1 = new Book();
-        book1.title = "W pustyni i w puszczy";
-        book1.author = "Henryk Sienkiewicz";
-        book1.releaseDate = 2010;
-        book1.pages = 296;
-        book1.publisher = "Greg";
-        book1.isbn = "9788373271890";
+        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890" );
+        Book book2 = new Book("Książka 2", "XYZ", 2000, 402, "Lion", "12346661234451" );
+        Book book3 = new Book("Książka 3", "ABC", 1992, 901, "Rat", "6612362317213" );
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        System.out.println(book1.title);
-        System.out.println(book1.author);
-        System.out.println(book1.releaseDate);
-        System.out.println(book1.pages);
-        System.out.println(book1.publisher);
-        System.out.println(book1.isbn);
+        book1.printInfo();
+        book2.printInfo();
+        book3.printInfo();
     }
 }
