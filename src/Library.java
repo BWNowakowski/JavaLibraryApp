178aa53605d82ab1a6f0f3e6220d3b0af7be5939
Bartosz Nowakowski <bartosz.wojciech.nowakowski@gmail.com>
2024-06-12 17:27:16 +0200

    4th commit konstruktory v2

--- src/Library.java
+++ src/Library.java
@@ -1,10 +1,10 @@
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.4";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890" );
-        Book book2 = new Book("Książka 2", "XYZ", 2000, 402, "Lion", "12346661234451" );
-        Book book3 = new Book("Książka 3", "ABC", 1992, 901, "Rat", "6612362317213" );
+        Book book2 = new Book("Książka 2", "Jacek Jackowski", 2000, 402, "Lion", "12346661234451" );
+        Book book3 = new Book("Książka 3", "Marek Markowski", 1992, 901, "Rat", "6612362317213" );
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
