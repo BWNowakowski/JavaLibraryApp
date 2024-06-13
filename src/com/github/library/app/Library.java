443bcc4906c6760c66896418057e3b2286b07a41
Bartosz Nowakowski <bartosz.wojciech.nowakowski@gmail.com>
2024-06-13 10:53:22 +0200

    zamiana poszczególnych zmiennych przy tworzeniu książek na elementy tablicy

--- src/com/github/library/app/Library.java
+++ src/com/github/library/app/Library.java
@@ -4,18 +4,20 @@ import com.github.library.model.Book;
 
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Biblioteka v0.6";
 
-        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890" );
-        Book book2 = new Book("Książka 2", "Jacek Jackowski", 2000, 402, "Lion", "12346661234451" );
-        Book book3 = new Book("Książka 3", "Marek Markowski", 1992, 901, "Rat", "6612362317213" );
+        Book[] books = new Book[1000];
+
+        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890" );
+        books[1] = new Book("Książka 2", "Jacek Jackowski", 2000, 402, "Lion", "12346661234451" );
+        books[2] = new Book("Książka 3", "Marek Markowski", 1992, 901, "Rat");
 
         System.out.println(appName);
-        System.out.println(book1.getAuthor());
+        System.out.println(books[1].getAuthor());
 //        book1.setAuthor("");
         System.out.println("Książki dostępne w bibliotece:");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
     }
 }
