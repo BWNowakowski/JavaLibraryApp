ad82a0d9f1f4cfb03d155873427b5414f91b4d4d
Bartosz Nowakowski <bartosz.wojciech.nowakowski@gmail.com>
2024-06-13 10:47:25 +0200

    specyfikatory dostępu, gettery i settery

--- src/com/github/library/app/Library.java
+++ src/com/github/library/app/Library.java
@@ -1,12 +1,18 @@
+package com.github.library.app;
+
+import com.github.library.model.Book;
+
 class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.4";
+        final String appName = "Biblioteka v0.5";
 
         Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890" );
         Book book2 = new Book("Książka 2", "Jacek Jackowski", 2000, 402, "Lion", "12346661234451" );
         Book book3 = new Book("Książka 3", "Marek Markowski", 1992, 901, "Rat", "6612362317213" );
 
         System.out.println(appName);
+        System.out.println(book1.getAuthor());
+//        book1.setAuthor("");
         System.out.println("Książki dostępne w bibliotece:");
         book1.printInfo();
         book2.printInfo();
