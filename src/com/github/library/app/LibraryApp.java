f72fb9d81cde8dd0cc43b5c478d5ba037c590ddf
Bartosz Nowakowski <bartosz.wojciech.nowakowski@gmail.com>
2024-06-19 15:50:44 +0200

    equals i toString

--- src/com/github/library/app/LibraryApp.java
+++ src/com/github/library/app/LibraryApp.java
@@ -1,7 +1,7 @@
 package com.github.library.app;
 
 public class LibraryApp {
-    private static final String APP_NAME = "Biblioteka v1.2";
+    private static final String APP_NAME = "Biblioteka v1.3";
 
 
     public static void main(String[] args) {
